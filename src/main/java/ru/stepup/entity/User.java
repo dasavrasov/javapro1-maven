1328043069a83ebbc067e8331a06f79360fc59bc
DSavrasov <a@a>
2024-10-07 01:17:13 +0300

    Задание 4

---
+++ src/main/java/ru/stepup/entity/User.java
@@ -0,0 +1,36 @@
+package ru.stepup.entity;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username")
+    private String name;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public User() {
+    }
+    public User(String name) {
+        this.name = name;
+    }
+}
